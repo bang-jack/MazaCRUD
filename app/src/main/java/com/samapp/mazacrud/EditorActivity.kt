bfa5864ecd8b3653818cf930d0a59d095c0989f7
bang-jack <zakyit531@gmail.com>
2025-02-13 10:52:55 +0700

    Updated for update function

--- app/src/main/java/com/samapp/mazacrud/EditorActivity.kt
+++ app/src/main/java/com/samapp/mazacrud/EditorActivity.kt
@@ -25,16 +25,39 @@ class EditorActivity : AppCompatActivity() {
 
         database = AppDatabase.getInstance(applicationContext)
 
+        var intent = intent.extras
+        if (intent!=null){
+            var id = intent.getInt("id", 0)
+            var user = database.userDao().get(id)
+
+            fullName.setText(user.fullName)
+            email.setText(user.email)
+            phone.setText(user.phone)
+        }
+
         btnsave.setOnClickListener {
             if (fullName.text.isNotEmpty() && email.text.isNotEmpty() && phone.text.isNotEmpty()){
-            database.userDao().insertAll(
-                User(
-                    null,
-                    fullName.text.toString(),
-                    email.text.toString(),
-                    phone.text.toString()
-                )
-            )
+                if(intent!=null){
+                    database.userDao().update(
+                        User(
+                            intent.getInt("id", 0),
+                            fullName.text.toString(),
+                            email.text.toString(),
+                            phone.text.toString()
+                        )
+                    )
+                }else{
+                    // tambah data
+                    database.userDao().insertAll(
+                        User(
+                            null,
+                            fullName.text.toString(),
+                            email.text.toString(),
+                            phone.text.toString()
+                        )
+                    )
+                }
+
                 finish()
         } else{
             Toast.makeText(
